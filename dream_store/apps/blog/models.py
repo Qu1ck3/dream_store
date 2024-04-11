86f08b4f7489bec84123ac620bb4e18c79bda2c1
Qu1ck3 <ganzaveretohad@gmail.com>
2024-04-11 16:39:20 +0300

    Tag model

--- dream_store/apps/blog/models.py
+++ dream_store/apps/blog/models.py
@@ -12,21 +12,32 @@ class BlogCategory(models.Model):
         verbose_name_plural = "Категории блога"
 
 
+class Tag(models.Model):
+    name = models.CharField(verbose_name="Тег", max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Тэг"
+        verbose_name_plural = "Тэги"
+
+
 class Article(models.Model):
-    category = models.ForeignKey(
-        to=BlogCategory,
-        verbose_name="Категория",
-        on_delete=models.SET_NULL,
-        null=True)
     title = models.CharField(verbose_name='Заголовок', max_length=255)
     text_preview = models.TextField(verbose_name='Текст-Превью')
     text = models.TextField(verbose_name='Текст')
     created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='Дата редактирования', auto_now=True)
+    tags = models.ManyToManyField(Tag, verbose_name="Тэги", blank=True)
+    category = models.ForeignKey(
+        to=BlogCategory,
+        verbose_name="Категория",
+        on_delete=models.SET_NULL,
+        null=True)
 
     def __str__(self):
         return self.title
-
 
     class Meta:
         verbose_name = 'Статья'
