ad437f7f71a253c7a5c70a61182fb4d16efa71cb
Qu1ck3 <ganzaveretohad@gmail.com>
2024-04-18 22:02:52 +0300

    user model

--- dream_store/config/settings.py
+++ dream_store/config/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'phonenumber_field',
     'apps.blog',
+    'apps.user',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,5 @@ MEDIA_ROOT = 'media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.User'
