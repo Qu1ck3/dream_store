0f437913942edefb8e79f47c3390c5e32eedb0d4
Qu1ck3 <ganzaveretohad@gmail.com>
2024-04-23 20:57:59 +0300

    product/product_to_category

--- dream_store/apps/catalog/admin.py
+++ dream_store/apps/catalog/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
-from apps.catalog.models import Category
+from apps.catalog.models import Category, Product, ProductCategory
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+
+
+class ProductCategoryInLine(admin.TabularInline):
+    model = Product.categories.through
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'quantity', 'created_at', 'updated_at')
+    list_display_links = ('id', 'name')
+    fields = ("name", "description", "price", "quantity")
+    readonly_fields = ("created_at", "updated_at")
+    inlines = [ProductCategoryInLine]
